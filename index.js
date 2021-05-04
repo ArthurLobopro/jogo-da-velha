3404a4824baac325bc5abae13c457ce5909aa191
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-05-04 09:30:11 -0300

    codigo retirado

--- index.js
+++ index.js
@@ -1,46 +1,5 @@
-// Classes dificuldade
-class Normal extends Easy{
-    // "Inteligencia" do bot, faz verificações em todos os angulos
-    vertical = (str,game) =>{
-        const sts = game.status
-        for( let i of range(0,3)){
-            if(sts[i]== str && sts[i+3] == str && sts[i+6] == '' ) { return i+6 }
-            if(sts[i]== str && sts[i+3] == ''  && sts[i+6] == str) { return i+3 }
-            if(sts[i]== ''  && sts[i+3] == str && sts[i+6] == str) { return i }
-        }
-        return ''
-    }
-    horizontal = (str,game) =>{
-        const sts = game.status
-        for( let i of range(0,7,3)){
-            if(sts[i] == str && sts[i+1] == str && sts[i+2] == '') { return i+2 }
-            if(sts[i] == str && sts[i+1] == '' && sts[i+2] == str) { return i+1 }
-            if(sts[i] == '' && sts[i+1] == str && sts[i+2] == str) { return i }
-        }
-        return  ''
-    }
-    diagonal = (str,game) =>{
-        const sts = game.status
-        if (sts[0] == str && sts[4] == str && sts[8] == '')  { return 8 }
-        if (sts[0] == str && sts[4] == ''  && sts[8] == str) { return 4 }
-        if (sts[0] == ''  && sts[4] == str && sts[8] == str) { return 0 }
-        if (sts[2] == str && sts[4] == str && sts[6] == '')  { return 6 }
-        if (sts[2] == str && sts[4] == ''  && sts[6] == str) { return 4 }
-        if (sts[2] == ''  && sts[4] == str && sts[6] == str) { return 2 }
-        return ''
-    }
-    play(game){
-        let jbot = this.vertical('o',game)
-        jbot = (jbot==='') ? this.horizontal('o',game) : jbot
-        jbot = (jbot==='') ? this.diagonal('o',game) : jbot
-        jbot = (jbot==='') ? this.vertical('x',game) : jbot
-        jbot = (jbot==='') ? this.diagonal('x',game) : jbot
-        jbot = (jbot==='') ? this.horizontal('x',game) : jbot
-        //Caso não há possibilidades nem de ganhar ou evitar a derrota, o bot faz uma jogada aleatória.
-        jbot = (jbot==='') ? randint(0,8) : jbot
-        return jbot
-    }
-}
+import Easy from "./scripts/dificulties/Easy.js"
+import Normal from "./scripts/dificulties/Normal.js"
 // Variáveis Globais
 const get = id => document.getElementById(id)
 const game = {
@@ -93,7 +52,7 @@ function jogadaPlayer(player,alerta=true) {
     }
 }
 function draw(id,src) {
-    get(`div${id}`).innerHTML=`<img src='midia/${src}.png' class='show${src}'>`
+    get(`div${id}`).innerHTML=`<img src='assets/${src}.png' class='show${src}'>`
 }
 function jogadaBot() {
     let jbot = bot.play(game)
