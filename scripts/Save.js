1dc002a3932c27113d752cd94a5681f26e58a46d
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-05-04 11:01:27 -0300

    Create Save.js

---
+++ scripts/Save.js
@@ -0,0 +1,33 @@
+function lerDificuldade(){
+    let difficulty = 'normal'
+    if(localStorage.difficulty == undefined){
+        criarDificuldade('normal')
+    }else{
+        ( { difficulty } = localStorage )
+    }
+
+    return difficulty
+}
+
+function criarDificuldade(dificuldade){
+    localStorage.difficulty = dificuldade
+    lerDificuldade()    
+}
+
+function lerPlacar(){
+    let data = {player: 0, bot: 0}
+    if(localStorage.placar == undefined){
+        criaPlacar(data)
+    }else{
+        data = JSON.parse(localStorage.placar)
+    }
+    return data
+}
+
+function criaPlacar(game){
+    let placar = game.placar
+    let json = JSON.stringify(placar)
+    localStorage.setItem("placar",json)
+}
+
+export { criarDificuldade, criaPlacar, lerDificuldade, lerPlacar }
